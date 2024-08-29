dd716796919edd24e1ea0f76fed1e8177cabb47f
Nicolas Kleiton <inicolaskleiton@gmail.com>
2024-08-28 22:06:27 -0300

    Classwork4: Nicolas Kleiton submission classwork 'romanos'

---
+++ classworks/classwork4/Nicolas_Kleiton/romanos.py
@@ -0,0 +1,30 @@
+def romanos (s):
+
+    dict = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+     }
+    
+    val = 0
+
+    for i in range(len(s)):
+        if i < len(s) - 1 and dict[s[i]] < dict[s[i + 1]]:
+            val -= dict[s[i]]
+        else:
+            val += dict[s[i]]
+    
+    return val
+
+s = 'III'
+print(romanos(s))
+
+s = "LVIII"
+print(romanos(s))
+
+s = "MCMXCIV"
+print(romanos(s))
